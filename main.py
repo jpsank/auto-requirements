1a63e6b4ce45edd9259041c1d601b93e1b9d9b20
puffyboa <julian@sankergroup.org>
2017-11-22 18:20:38 -0600

    make virtualenv along with requirements.txt

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import os
 import ast
-from subprocess import Popen, PIPE
+import subprocess
 import sys
 
 
 class REQER:
     def __init__(self,REQ_TXT_FN="requirements.txt"):
-        self.MODULES = Popen("pip freeze", stdout=PIPE).stdout.read().decode().replace("\r\n", "\n")
+        self.MODULES = subprocess.Popen("pip freeze", stdout=subprocess.PIPE).stdout.read().decode().replace("\r\n", "\n")
         self.MODULES = [m.split("==") for m in self.MODULES.split("\n") if "==" in m]
         self.MODULES = {p[0]:p[1] for p in self.MODULES}
 
@@ -44,21 +44,23 @@ class REQER:
         modules = ["%s==%s" % (m,self.MODULES[m]) for m in list(set(modules)) if m in self.MODULES]
         return modules
 
-    def mk_requirementsTXT(self,path,fn=None):
+    def mk_requirementsTXT(self,path,fn=None, venv=False):
         fn = fn if fn is not None else self.REQ_TXT_FN
         required = self.get_requirements(path)
         print(required)
         if required:
             with open(os.path.join(path,fn),"w") as f:
                 f.write('\n'.join(required))
+            if venv:
+                self.mk_venv(path,"{}_venv".format(os.path.split(path)[-1]),fn)
 
-    def recur_mk_requirementsTXT(self,container_path,fn=None):
+    def batch_mk_requirementsTXT(self, container_path, fn=None, venv=False):
         fn = fn if fn is not None else self.REQ_TXT_FN
         for project in os.listdir(container_path):
             joined = os.path.join(container_path, project)
             if os.path.isdir(joined):
                 print(project)
-                self.mk_requirementsTXT(joined, fn)
+                self.mk_requirementsTXT(joined, fn, venv)
 
 
     def rm_requirementsTXT(self,path,fn=None):
@@ -67,7 +69,7 @@ class REQER:
         if os.path.isfile(joined):
             os.remove(joined)
 
-    def recur_rm_requirementsTXT(self,container_path,fn=None):
+    def batch_rm_requirementsTXT(self, container_path, fn=None):
         fn = fn if fn is not None else self.REQ_TXT_FN
         for project in os.listdir(container_path):
             joined = os.path.join(container_path,project)
@@ -75,7 +77,25 @@ class REQER:
                 self.rm_requirementsTXT(joined,fn)
 
 
+    def mk_venv(self,path,name,reqfn=None):
+        reqfn = reqfn if reqfn is not None else self.REQ_TXT_FN
+        if sys.platform == "win32":
+            commands = ["pip install virtualenvwrapper-win",
+                        "cd %s" % path,
+                        "mkvirtualenv %s" % name,
+                        "workon %s" % name,
+                        "pip install -r %s" % reqfn]
+            subprocess.check_call(' && '.join(commands), shell=True)
+        else:
+            commands = ["pip install virtualenv",
+                        "cd %s" % path,
+                        "virtualenv %s" % name,
+                        "source %s/bin/activate" % name,
+                        "pip install -r %s" % reqfn]
+            subprocess.check_call(' && '.join(commands), shell=True)
+
+
 req = REQER()
 
-req.recur_mk_requirementsTXT(r"C:\Users\julia\PycharmProjects")
-
+#req.batch_mk_requirementsTXT(r"C:\Users\user\PycharmProjects",venv=True)
+req.mk_requirementsTXT(r"C:\Users\user\Documents\GitHub\auto-requirements",venv=True)
